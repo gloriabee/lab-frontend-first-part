028511c8128d23aa94c343597c98f114d8d08267
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2024-10-09 11:29:39 +0700

    adding organizer card

---
+++ src/components/OrganizerCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { type Organizer } from '@/types';
+defineProps<{
+    organizer: Organizer
+}>()
+</script>
+
+<template>
+     <div class="cursor-pointer border border-gray-600 p-[20px] w-[250px] mb-[18px] hover:scale-101 hover:shadow-sp">
+      <h2>{{ organizer.name }}</h2>
+      <h3>{{ organizer.address }}</h3>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
