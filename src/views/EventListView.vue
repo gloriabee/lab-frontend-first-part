a74cd9c1fd3ac5cba172b4b7af305e59646128b7
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2024-10-10 21:14:49 +0700

    able to query the data via search box

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -4,6 +4,7 @@ import { type Event } from '@/types'
 import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
 import { useRouter } from 'vue-router'
+import BaseInput from '@/components/BaseInput.vue'
 
 const router = useRouter()
 const events = ref<Event[] | null>(null)
@@ -31,12 +32,43 @@ onMounted(() => {
       })
   })
 })
+
+const keyword=ref('')
+function updateKeyword(value:string){
+  let queryFunction;
+  if(keyword.value===''){
+    queryFunction=EventService.getEvents(3,page.value)
+  }
+  else{
+    queryFunction=EventService.getEventsByKeyword(keyword.value,3,page.value)
+  }
+  queryFunction
+  .then((response)=>{
+    events.value=response.data
+    console.log('events',events.value);
+    totalEvents.value=response.headers['x-total-count']
+    console.log('totalEvent',totalEvents.value);
+  })
+  .catch(()=>{
+    router.push({
+      name:'network-error-view'
+    })
+  })
+}
 </script>
 
 <template>
   <h1>Events For Good</h1>
   <!-- new element -->
   <div class="flex flex-col items-center">
+    <div class="w-64">
+      <BaseInput
+      v-model="keyword"
+      type="text"
+      label="Search..."
+      @input="updateKeyword"
+      class="w-full" />
+    </div>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
       <RouterLink
